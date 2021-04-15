9e66ea0cef0f70a528d51bbb4c8e58eb02c6f8fd
Julia <juliancamilohermida@hotmail.com>
2021-04-15 16:36:28 -0500

    logout_view en users añadido

--- Platzigram/Platzigram/urls.py
+++ Platzigram/Platzigram/urls.py
@@ -13,9 +13,11 @@ from users import views as users_views
 
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path("hello-world/", local_views.hello_world, name="hello_worlds"),
     path("posts/", posts_views.list_posts, name="feed"),
-    path("users/login", users_views.login_view, name="login" )
+    path("users/login/", users_views.login_view, name="login" ),
+    path("users/logout/", users_views.logout_view, name="logout"),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Platzigram/users/views.py
+++ Platzigram/users/views.py
@@ -2,7 +2,9 @@
 
 # Django
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+
 
 def login_view(request):
     """Login view."""
@@ -10,10 +12,17 @@ def login_view(request):
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
-        
+
         if user:
             login(request, user)
             return redirect("feed")
         else:
             return render(request, "users/login.html", {"error": "Invalid username and password"})
     return render(request, "users/login.html")
+
+
+@login_required
+def logout_view(request):
+    """Logout view."""
+    logout(request)
+    return redirect("login")
