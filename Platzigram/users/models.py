ddce92ec04ca4d2f9dbd63e8c2d233073da6c863
Julia <juliancamilohermida@hotmail.com>
2021-05-01 16:22:08 -0500

    Atributo count_post añadido.

--- Platzigram/users/models.py
+++ Platzigram/users/models.py
@@ -7,21 +7,20 @@ from django.db import models
 
 class Profile(models.Model):
     """Profile model.
-
-     Model 1:1 with User"""
+    Model 1:1 with User"""
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     website = models.URLField(max_length=200, blank=True)
     biography = models.TextField(blank=True)
     phone_number = models.CharField(max_length=20, blank=True)
 
-
     picture = models.ImageField(
         upload_to="users/pictures", 
         blank=True, 
         null=True
         )
-
+        
+    count_post = models.IntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
@@ -32,8 +31,7 @@ class Profile(models.Model):
 
 class Follow(models.Model):
     """Follow model.
-
-        total following and followers"""
+    total following and followers"""
 
     follower = models.IntegerField(default=0)
     following = models.IntegerField(default=0)
